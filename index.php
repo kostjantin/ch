47f16d3ba07ced5aed7564b06f977eb50849bac1
Kostiantin Muliar <kostjantin@gmail.com>
2021-02-06 15:41:09 +0200

    Correct random values

--- index.php
+++ index.php
@@ -9,13 +9,14 @@ $font = 'arial.ttf';
 
 $captchaValue = (string)rand(10000, 99999);
 for ($i = 0; $i < strlen($captchaValue); $i++) {
-    $textColor = imagecolorallocate($im, rand(0,200), rand(0,200), rand(0,200));
+    $textColor = imagecolorallocate($im, rand(0,155), rand(0,155), rand(0,155));
 
     imagettftext(
         $im,
-        30 + rand(-7, 5),
+        30 + rand(-10, 5),
         0 + rand(-250, 250) / 10,
-        $i * 40 + 5, 40,
+        $i * rand(35,40) + 5,
+        40 + rand(-5,5),
         $textColor,
         $font,
         $captchaValue[$i]
