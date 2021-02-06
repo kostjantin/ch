caef9d0c6b4ef1d05a0eade451902a956acb3a9e
Kostiantin Muliar <kostjantin@gmail.com>
2021-02-06 15:33:35 +0200

    Second version of image generator for captcha

--- index.php
+++ index.php
@@ -4,13 +4,22 @@ require_once "vendor/autoload.php";
 header("Content-Type: image/png");
 
 $im = @imagecreate(200, 50);
-$background_color = imagecolorallocate($im, 0, 0, 0);
-$text_color = imagecolorallocate($im, 233, 14, 91);
+$backgroundColor = imagecolorallocate($im, 255, 255, 255);
 $font = 'arial.ttf';
 
 $captchaValue = (string)rand(10000, 99999);
 for ($i = 0; $i < strlen($captchaValue); $i++) {
-    imagettftext($im, 30, 0, $i * 30 + 30, 40, $text_color, $font, $captchaValue[$i]);
+    $textColor = imagecolorallocate($im, rand(0,200), rand(0,200), rand(0,200));
+
+    imagettftext(
+        $im,
+        30 + rand(-7, 5),
+        0 + rand(-250, 250) / 10,
+        $i * 40 + 5, 40,
+        $textColor,
+        $font,
+        $captchaValue[$i]
+    );
 }
 
 imagepng($im);
